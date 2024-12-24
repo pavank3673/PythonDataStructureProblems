90d37b1f52697c985df223b4da3cec9bc9773071
pavank3673 <pavank3673@outlook.com>
2024-12-24 11:04:19 +0530

    [Pavan Kumar]Added:tuple problem 6

---
+++ day1/tuple_6.py
@@ -0,0 +1,8 @@
+tu = (10, 20, 30, 40, 20, 50, 20)
+
+element = 20
+
+if element in tu:
+    print(f"Element {element} exists in {tu}")
+else:
+    print(f"Element {element} does not exists in {tu}")
