e2e8c0f00e81b9b73b37c7310c7fc162f56ac598
pavank3673 <pavank3673@outlook.com>
2024-12-24 11:02:37 +0530

    [Pavan Kumar]Added:set problem 6

---
+++ day1/set_6.py
@@ -0,0 +1,6 @@
+left_set =  {"apple", 20, 30, False, "e"}
+right_set = { False, "e", 22.5, True}
+
+res_set = left_set.intersection(right_set)
+
+print(f"Intersection of sets {left_set} & {right_set} are : {res_set}")
