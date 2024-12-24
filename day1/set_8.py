cb8ae51755f45929b7b9e33e77ea5d36dc75925e
pavank3673 <pavank3673@outlook.com>
2024-12-24 11:02:50 +0530

    [Pavan Kumar]Added:set problem 8

---
+++ day1/set_8.py
@@ -0,0 +1,6 @@
+left_set =  {"apple", 20, 30, False, "e"}
+right_set = { False, "e", 22.5, True}
+
+res_set = left_set.difference(right_set)
+
+print(f"Difference of sets {left_set} & {right_set} are : {res_set}")
