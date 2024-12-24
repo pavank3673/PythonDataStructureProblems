d96697ca986ae00918b7cec9b6277c93f8a79822
pavank3673 <pavank3673@outlook.com>
2024-12-24 11:02:44 +0530

    [Pavan Kumar]Added:set problem 7

---
+++ day1/set_7.py
@@ -0,0 +1,6 @@
+left_set =  {"apple", 20, 30, False, "e"}
+right_set = { False, "e", 22.5, True}
+
+res_set = left_set.union(right_set)
+
+print(f"Union of sets {left_set} & {right_set} are : {res_set}")
