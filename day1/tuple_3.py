7db49c53224cd9852f00b466442d0f8b17c0c8fd
pavank3673 <pavank3673@outlook.com>
2024-12-24 11:04:01 +0530

    [Pavan Kumar]Added:tuple problem 3

---
+++ day1/tuple_3.py
@@ -0,0 +1,5 @@
+tu = ("apple", 20, 30, False, "e", 22.5, True)
+
+(fruit, num1, num2, bool1, char1, double1, bool2) = tu
+
+print(f"Unpacked tuple variables : {fruit}, {num1}, {num2}, {bool1}, {char1}, {double1}, {bool2}")
