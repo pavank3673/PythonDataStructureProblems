aacf8e567baefc22ea296071e944d03ce278e1f4
pavank3673 <pavank3673@outlook.com>
2024-12-26 10:43:13 +0530

    [Pavan Kumar]Added:dictionary table program

---
+++ day2/dict_9_dictionary_table.py
@@ -0,0 +1,13 @@
+marks = {
+    "English":60,
+    "Science":55,
+    "Biology":70,
+    "Physics":50
+}
+print("+------------+------------+")
+print("| Subject    | Marks      |")
+print("+------------+------------+")
+for x, y in marks.items():
+    print(f"| {x}    | {y}         |")
+
+print("+------------+------------+")
